9edb4363b2dfc93e4aded8bd018e08be84300a21
Andi DeVois <adevois@gmail.com>
2017-03-04 12:06:41 -0600

    Set up models and properties

---
+++ FakeBlog/Models/Author.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeBlog.Models
+{
+    public class Author
+    {
+        [Key]
+        public int AuthorId { get; set; }
+
+        public int Username { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string PasswordHash { get; set; }
+
+        public List<BlogPost> BlogPosts { get; set; }
+    }
+}
---
+++ FakeBlog/Models/BlogPost.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FakeBlog.Models
+{
+    public class BlogPost
+    {
+        [Key]
+        public int BlogPostId { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        public string Title { get; set; }  
+              
+        public DateTime CreationDate { get; set; } //Required by default
+        public DateTime PublishedDate { get; set; }
+        public string Content { get; set; }
+        public bool IsDraft { get; set; }
+        public bool Edited { get; set; }
+        public string URL { get; set; }
+
+
+    }
+}
